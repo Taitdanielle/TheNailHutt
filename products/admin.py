349553fa76f0f8d9c1df6a527bd1cd0e4271b29a
Danielle Tait <Danielletait@hotmail.co.uk>
2020-12-04 23:54:11 +0000

    added , to list display in admin.py

--- products/admin.py
+++ products/admin.py
@@ -2,21 +2,23 @@ from django.contrib import admin
 from .models import Product, Category
 
 # Register your models here.
-class ProductAdmin(admin.ModelAdmin):
-        list_display = (
-            'sku'
-            'name'
-            'category'
-            'price'
-            'image'
-        )
 
-        ordering = ('sku',)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'image',
+    )
+
+    ordering = ('sku',)
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = (
-        'friendly_name'
-        'name'
+        'friendly_name',
+        'name',
     )
+
 admin.site.register(Product, ProductAdmin)
-admin.site.register(Category,CategoryAdmin)
+admin.site.register(Category, CategoryAdmin)
