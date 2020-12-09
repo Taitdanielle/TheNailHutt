c47c88b639c4918c1c305fa60a7f4a43bc28ba2d
Danielle Tait <Danielletait@hotmail.co.uk>
2020-12-09 21:09:25 +0000

    created landing app

--- landing/views.py
+++ landing/views.py
@@ -1,9 +1,3 @@
 from django.shortcuts import render
 
-
 # Create your views here.
-def index(request):
-    """
-    A view to return landing page
-    """
-    return render(request, 'home/index.html')
