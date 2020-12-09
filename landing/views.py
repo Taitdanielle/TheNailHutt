52bb527ae7d0aad8b563298fca48c06ba8b478f1
Danielle Tait <Danielletait@hotmail.co.uk>
2020-12-09 21:18:48 +0000

    added views and urls to landing

--- landing/views.py
+++ landing/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    """
+    A view to return landing page
+    """
+    return render(request, 'landing/index.html')
