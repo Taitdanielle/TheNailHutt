cbee5a634165606ca137b91556b0f3dfdb50815d
Danielle Tait <Danielletait@hotmail.co.uk>
2020-12-09 14:41:45 +0000

    added imports for log in required

--- products/views.py
+++ products/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.db.models.functions import Lower
 
 
 def all_products(request):
