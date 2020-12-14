5cc8eb1b0ce44775a3c3a0c73f4b275a1a788bf7
Danielle Tait <Danielletait@hotmail.co.uk>
2020-12-14 19:33:31 +0000

    added gallery image model

--- landing/models.py
+++ landing/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class GalleryImage(models.Model):
+
+    name = models.CharField(max_length=254)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
