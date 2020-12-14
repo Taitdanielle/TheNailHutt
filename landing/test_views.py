72f3d01d2aaf371da1f6d4fd26243cb005d0f06b
Danielle Tait <Danielletait@hotmail.co.uk>
2020-12-14 22:08:59 +0000

    added some test views

---
+++ landing/test_views.py
@@ -0,0 +1,12 @@
+from django.test import TestCase    
+
+class TestLandingViews(TestCase):
+
+    def test_get_landing_page(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "landing/includes/image__buttons.html")
+        self.assertTemplateUsed(response, "landing/includes/landing_partys.html")
+        self.assertTemplateUsed(response, "landing/includes/landing_products.html")
+        self.assertTemplateUsed(response, "landing/includes/landing_quote.html")
+        self.assertTemplateUsed(response, "landing/includes/landing_services.html")
