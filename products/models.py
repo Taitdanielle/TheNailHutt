69a483531acacd9ee344f1ca84316133b150dc74
Danielle Tait <Danielletait@hotmail.co.uk>
2020-12-14 22:08:21 +0000

    added services model

--- products/models.py
+++ products/models.py
@@ -11,6 +11,18 @@ class Category(models.Model):
     def get_freiendly_name(self):
         return self.friendly_name
 
+class Services(models.Model):
+
+    class Meta:
+        verbose_name_plural ='services'
+
+    name = models.CharField(max_length=254, null=True)
+    service = models.OneToOneField('Product', null=True, blank=True, 
+                                    on_delete=models.CASCADE)    
+
+    def __str__(self):
+        return self.name                               
+
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
