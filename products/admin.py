5553b783e2daf6ee9d893c8aed28b81bd0979906
Danielle Tait <Danielletait@hotmail.co.uk>
2020-12-16 18:39:52 +0000

    added services admin

--- products/admin.py
+++ products/admin.py
@@ -21,6 +21,13 @@ class CategoryAdmin(admin.ModelAdmin):
         'name',
     )
 
+class ServicesAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'service',
+    )    
+
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Services, ServicesAdmin)
