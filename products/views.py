5cd8d7607ca26ad86cdc60bd0e756ba39a04125c
Danielle Tait <Danielletait@hotmail.co.uk>
2020-12-22 18:01:24 +0000

    removed services from products views

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
-from .models import Product, Category, Services
+from .models import Product, Category
 from django.db.models.functions import Lower
 
 
