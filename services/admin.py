14ee0d6d809c1e54346574ea37c6f24372713ed0
Danielle Tait <Danielletait@hotmail.co.uk>
2020-12-24 23:43:07 +0000

    added services to adin/servies.py

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
+
+class ServicesAdmin(admin.ModelAdmin):
+    list_display = (
+        'service',
+        'description',
+        'price',
+        'is_a_service',
+        'image',
+        'image_url'
+    )
+
+class ServicesDetailAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'service',
+    )
+
+admin.site.register(Services, ServicesAdmin)
+admin.site.register(Services, ServicesDetailAdmin)
