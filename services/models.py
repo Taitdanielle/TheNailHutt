32e3572d2cbb836cdf58ab0d01729f474e0068f0
Danielle Tait <Danielletait@hotmail.co.uk>
2020-12-26 21:55:23 +0000

    added services model

--- services/models.py
+++ services/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Services(models.Model):
+
+    class Meta:
+        verbose_name_plural ='Services'
+
+    name = models.CharField(max_length=254, null=True)
+    friendly_name = models.OneToOneField('Services', null=True, blank=True, 
+                                    on_delete=models.CASCADE)    
+
+    def __str__(self):
+        return self.name
+
+class ServicesDetail(models.Model):
+    service = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    is_a_service = models.BooleanField(default=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    class Meta:
+        ordering = ["pk"]
+
+    def __str__(self):
+        return self.name        
