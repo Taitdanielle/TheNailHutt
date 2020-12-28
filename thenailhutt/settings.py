a46396ae6e3478d5ea1ef69959ed4fbf97415b08
Danielle Tait <Danielletait@hotmail.co.uk>
2020-12-28 19:24:46 +0000

    deployment part 2

--- thenailhutt/settings.py
+++ thenailhutt/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = ')7r$7xg$1zlkvhhvk_6x33i2=4mw3lf!gr*dcfc=sydtmzz+_g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['the-nail-hutt.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -83,6 +83,7 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.static',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
@@ -125,12 +126,18 @@ WSGI_APPLICATION = 'thenailhutt.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABSE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
- }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
