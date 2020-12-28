173c8ead5d96c9d0ee3c0f76b6846b348e769e77
Danielle Tait <Danielletait@hotmail.co.uk>
2020-12-28 17:24:57 +0000

    added servics to views/bag

--- bag/views.py
+++ bag/views.py
@@ -3,7 +3,6 @@ from django.contrib import messages
 
 from products.models import Product
 
-
 # Create your views here.
 
 def view_bag(request):
@@ -15,27 +14,12 @@ def add_to_bag(request, item_id):
     product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    size = None
-    if 'product_size' in request.POST:
-        size = request.POST['product_size']
     bag = request.session.get('bag', {})
-
-    if size:
-        if item_id in list(bag.keys()):
-            if size in bag[item_id]['items_by_size'].keys():
-                bag[item_id]['items_by_size'][size] += quantity
-                messages.success(request, f'Updated size {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]}')
-            else:
-                bag[item_id]['items_by_size'][size] = quantity
-                messages.success(request, f'Added size {size.upper()} {product.name} to your bag')
-        else:
-            bag[item_id] = {'items_by_size': {size: quantity}}
-            messages.success(request, f'Added size {size.upper()} {product.name} to your bag')
-    else:
-        if item_id in list(bag.keys()):
+    
+    if item_id in list(bag.keys()):
             bag[item_id] += quantity
             messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
-        else:
+    else:
             bag[item_id] = quantity
             messages.success(request, f'Added {product.name} to your bag')
 
@@ -46,8 +30,6 @@ def adjust_bag(request, item_id):
     """Adjust the quantity of specified amount"""
 
     product = get_object_or_404(Product, pk=item_id)
+    service = get_object_or_404(Service, pk=item_id)
     quantity = int(request.POST.get('quantity'))
-    size = None
-    if 'product_size' in request.POST:
-        size = request.POST['product_size']
     bag = request.session.get('bag', {})    
