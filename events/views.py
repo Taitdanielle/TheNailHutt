540e4e8a0da48b8c0ca4984d49f4d83a874d8b4a
Danielle Tait <Danielletait@hotmail.co.uk>
2020-12-29 23:23:16 +0000

    added events views

--- events/views.py
+++ events/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Event
 
-# Create your views here.
+
+def events(request):
+    """
+    A view to return events page, getting event items(day,time and description)
+    from the Event model
+    """
+    events = Event.objects.all()
+    context = {
+        'events': events,
+    }
+    return render(request, 'events/events.html', context)
