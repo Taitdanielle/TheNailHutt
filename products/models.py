299ff7ab2498b12abf214f10d706d89b3155df3e
Danielle Tait <Danielletait@hotmail.co.uk>
2020-12-30 16:47:05 +0000

    removed is a service from products models

--- products/models.py
+++ products/models.py
@@ -17,7 +17,6 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    is_a_service = models.BooleanField(default=False)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
