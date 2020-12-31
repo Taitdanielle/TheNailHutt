ad78a48d26259b0ceca91778ffd10bdce2a32513
Danielle Tait <Danielletait@hotmail.co.uk>
2020-12-31 16:38:43 +0000

    added aws cache control

--- thenailhutt/settings.py
+++ thenailhutt/settings.py
@@ -186,6 +186,12 @@ MEDIA_URL ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # chache control
+    AWS_S3_OBJECT_PARAMATERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=9460800',
+    }
+    
     # bucket config
     AWS_STORAGE_BUCKET_NAME = 'the-nail-hutt'
     AWS_S3_REGION_NAME = 'eu-west-1'
