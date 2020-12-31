d8de159a62aaada267f9b525db88ab16b1e28712
Danielle Tait <Danielletait@hotmail.co.uk>
2020-12-31 14:07:04 +0000

    added add products url to project manage,met

--- profiles/views.py
+++ profiles/views.py
@@ -11,34 +11,37 @@ def profile(request):
     profile = get_object_or_404(UserProfile, user=request.user)
 
     if request.method == 'POST':
-        form= UserProfileForm(request.POST, isinstance=profile)
+        form = UserProfileForm(request.POST, instance=profile)
         if form.is_valid():
             form.save()
-            messeges.success(request, 'Profile Updated Successfully')
-
-    form = UserProfileForm(isinstance=profile)
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request, 'Update failed. Please ensure the form is valid.')
+    else:
+        form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
     context = {
-        'form': form, 
+        'form': form,
         'orders': orders,
         'on_profile_page': True
     }
 
     return render(request, template, context)
 
-def order_histroy(request, order_number):
-    order = get_object_or_404(Order, order_number=order_number)    
 
-    messages.info(request,(
-        f'This is a past confirmation for order {order_number}. '
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
         'A confirmation email was sent on the order date.'
     ))
 
-    template = 'checkout/checkout_seccess.html'
+    template = 'checkout/checkout_success.html'
     context = {
-        'order': order, 
+        'order': order,
         'from_profile': True,
     }
 
