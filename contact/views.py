39dc28197543142c0fbff5dfc98b3f57378b5943
Danielle Tait <Danielletait@hotmail.co.uk>
2020-12-31 16:03:10 +0000

    added user profile to contact

--- contact/views.py
+++ contact/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.core.mail import send_mail
 from django.conf import settings
 from .forms import ContactForm
-
+from profiles.models import UserProfile
 
 
 def contact(request):
